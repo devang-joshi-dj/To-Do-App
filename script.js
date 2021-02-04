954097b508c1e2126eebc1494daeef3d41a7b231
Devang Joshi <devangjoshi11@gmail.com>
2021-02-04 17:36:32 +0530

    Added Responsiveness

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 window.onload = () => {
     document.getElementById("task").value = "";
     document.getElementById("task").focus();
+
     document.getElementById("addTask").addEventListener("click", addTask);
     document.getElementById("task").addEventListener("keyup", function (event) {
         // Number 13 is the "Enter" key on the keyboard
@@ -17,36 +18,47 @@ let i = 0;
 var tasks = [];
 
 const addTask = () => {
-    var task = document.getElementById("task").value;
+    let task = document.getElementById("task").value;
     task = task.trim();
+    task = task.charAt(0).toUpperCase() + task.slice(1);
     if (task != "") {
         if (!tasks.includes(task)) {
             tasks.push(task);
-            var todoItem = document.createElement("li");
-            var addValue = document.createTextNode(task);
-            todoItem.className = "addedTodo";
-            todoItem.appendChild(addValue);
 
-            var todoRemove = document.createElement("span");
-            var removeValue = document.createTextNode("Close");
+            const todoItemValue = document.createElement("span");
+            todoItemValue.className = "addedTodoValue";
+            const addValue = document.createTextNode(task);
+            todoItemValue.appendChild(addValue);
+
+            const todoRemove = document.createElement("div");
+            const todoRemoveElement = document.createElement("span")
+            const todoRemoveElementValue = document.createTextNode("Done");
+            todoRemoveElement.appendChild(todoRemoveElementValue);
             todoRemove.className = "removeTodo";
-            todoRemove.appendChild(removeValue);
+            todoRemove.appendChild(todoRemoveElement);
 
+            const todoItem = document.createElement("li");
+            todoItem.className = "addedTodo";
+            todoItem.appendChild(todoItemValue);
             todoItem.appendChild(todoRemove);
 
-            var element = document.getElementById("todoItems");
+            const element = document.getElementById("todoItems");
             element.appendChild(todoItem);
 
             document.getElementsByClassName("removeTodo")[i].addEventListener("click", function () {
                 this.parentNode.remove();
                 i--;
-                var index = tasks.indexOf(task);
+                const index = tasks.indexOf(task);
                 tasks.splice(index, 1);
 
                 document.getElementById("task").focus();
             }, false);
             i++;
             document.getElementById("task").value = "";
+        }
+        else {
+            alert("Task Already Exists");
+            document.getElementById("task").select();
         }
     }
     document.getElementById("task").focus();
